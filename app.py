ef5d41ef312ffa2ce128fa332f013766b7458a83
Sergio Lucero <sergiolucero@gmail.com>
2022-08-01 05:07:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ import pandas as pd
 df=pd.read_csv('whs2021.csv')
 st.header('UNESCO World Heritage Sites')
 st.dataframe(df)
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.dataframe(df.Region.value_counts())
+  
+with col2:
+    st.dataframe(df['Country name'].value_counts())
